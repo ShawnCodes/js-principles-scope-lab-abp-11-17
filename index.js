053584e997d945203c384ba365aff996c2c14027
Shawn T <shawn@huthunt.com>
2017-11-21 04:56:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function setBestCustomer() {
 function overwriteBestCustomer() {
   bestCustomer = 'maybe bob'
 }
+
+const leastFavoriteCustomer = 'Tim'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'not Tim'
+}
